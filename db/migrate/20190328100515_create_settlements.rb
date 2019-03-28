fb1ff39c3bb2cf61bfb1e596740da7e2ddc9bdcd
Elif Mentes <e.mentes88@gmail.com>
2019-03-28 10:06:03 +0000

    owner_id added without migration complications

---
+++ db/migrate/20190328100515_create_settlements.rb
@@ -0,0 +1,10 @@
+class CreateSettlements < ActiveRecord::Migration[5.2]
+  def change
+    create_table :settlements do |t|
+      t.references :user, foreign_key: true
+      t.references :business, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
